eb9ab2211522133c4ccab33fabf370c325e0578c
omodauda <omodauda.dl@gmail.com>
2026-01-25 21:19:31 +0100

    disable dynamic colors

--- app/src/main/java/com/example/splitwise/ui/theme/Theme.kt
+++ app/src/main/java/com/example/splitwise/ui/theme/Theme.kt
@@ -116,7 +116,7 @@ private val DarkColorScheme = darkColorScheme(
 fun SplitWiseTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
