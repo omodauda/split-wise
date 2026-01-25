9f018311c47be55b278610739ed0a970975aa3d6
omodauda <omodauda.dl@gmail.com>
2026-01-25 21:20:58 +0100

    onboarding data

---
+++ app/src/main/java/com/example/splitwise/data/onboarding.kt
@@ -0,0 +1,31 @@
+package com.example.splitwise.data
+
+import com.example.splitwise.R
+import com.example.splitwise.model.OnboardingData
+
+val onboardingData = listOf(
+    OnboardingData(
+        image = R.drawable.onboarding_one,
+        titles = R.array.onboarding_first_titles,
+        title2 = R.array.onboarding_second_titles,
+        description = R.array.descriptions
+    ),
+    OnboardingData(
+        image = R.drawable.onboarding_two,
+        titles = R.array.onboarding_first_titles,
+        title2 = R.array.onboarding_second_titles,
+        description = R.array.descriptions
+    ),
+    OnboardingData(
+        image = R.drawable.onboarding_three,
+        titles = R.array.onboarding_first_titles,
+        title2 = R.array.onboarding_second_titles,
+        description = R.array.descriptions
+    ),
+    OnboardingData(
+        image = R.drawable.onboarding_four,
+        titles = R.array.onboarding_first_titles,
+        title2 = R.array.onboarding_second_titles,
+        description = R.array.descriptions
+    )
+)
