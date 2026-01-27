fa217427406fbdad7d80a43a67fe9421d8a3eb40
omodauda <omodauda.dl@gmail.com>
2026-01-27 20:57:07 +0100

    home bottom tab

--- app/src/main/java/com/example/splitwise/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/splitwise/ui/navigation/Screen.kt
@@ -5,4 +5,10 @@ sealed class Screen(val route: String) {
     object Login: Screen("Login")
     object Signup: Screen("Signup")
     object ForgotPassword: Screen("ForgotPassword")
+
+    object Home: Screen("Home")
+    object Groups: Screen(route = "Groups")
+    object Activity: Screen(route = "Activity")
+    object Friends: Screen(route = "Friends")
+    object Profile: Screen(route = "Profile")
 }
