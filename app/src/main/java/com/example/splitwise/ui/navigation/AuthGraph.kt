838a40aecc02049450cec697eb9c1ce0b94058fa
omodauda <omodauda.dl@gmail.com>
2026-01-27 16:09:01 +0100

    auth nav graph

---
+++ app/src/main/java/com/example/splitwise/ui/navigation/AuthGraph.kt
@@ -0,0 +1,35 @@
+package com.example.splitwise.ui.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.example.splitwise.ui.features.auth.login.LoginScreen
+import com.example.splitwise.ui.features.auth.onboarding.OnboardingScreen
+import com.example.splitwise.ui.features.auth.signup.SignupScreen
+
+fun NavGraphBuilder.authNavGraph(
+    navController: NavHostController
+) {
+    navigation(
+        startDestination = Screen.Onboarding.route,
+        route = "auth_graph"
+    ) {
+        composable(route = Screen.Onboarding.route) {
+            OnboardingScreen(
+                goToLogin = {navController.navigate(Screen.Login.route)}
+            )
+        }
+        composable(route = Screen.Login.route) {
+            LoginScreen(
+                goToSignup = {navController.navigate(Screen.Signup.route)}
+            )
+        }
+        composable(route = Screen.Signup.route) {
+            SignupScreen(
+               goBack = {navController.popBackStack()}
+            )
+
+        }
+    }
+}
