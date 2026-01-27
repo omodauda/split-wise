51dfbf5b673f9552f496faaee78453559ff34d1e
omodauda <omodauda.dl@gmail.com>
2026-01-27 20:24:55 +0100

    auth view model

---
+++ app/src/main/java/com/example/splitwise/ui/features/auth/AuthViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.splitwise.ui.features.auth
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.splitwise.data.repository.AuthRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class AuthViewModel(private val repo: AuthRepository): ViewModel() {
+    val isAuthenticated = repo.isAuthenticated
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = null
+        )
+
+    fun authenticate() {
+        viewModelScope.launch {
+            repo.authenticate()
+        }
+    }
+}
