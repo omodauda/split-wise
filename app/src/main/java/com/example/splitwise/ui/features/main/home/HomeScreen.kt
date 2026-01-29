311bba48fe68bfb1ca7fadf8d4b3ddedff95084a
omodauda <omodauda.dl@gmail.com>
2026-01-30 00:53:09 +0100

    homescreen

--- app/src/main/java/com/example/splitwise/ui/features/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/splitwise/ui/features/main/home/HomeScreen.kt
@@ -1,33 +1,28 @@
 package com.example.splitwise.ui.features.main.home
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.splitwise.R
-import com.example.splitwise.ui.theme.CurrencyLarge
+import com.example.splitwise.ui.components.AppIconTextButton
+import com.example.splitwise.ui.features.main.home.components.DashBoard
+import com.example.splitwise.ui.features.main.home.components.OwedView
+import com.example.splitwise.ui.features.main.home.components.OwingView
 import com.example.splitwise.ui.theme.ScreenDimensions
 import com.example.splitwise.ui.theme.Spacing
-import com.example.splitwise.ui.theme.SplitWiseShapes
 import com.example.splitwise.ui.theme.SplitWiseTheme
-import com.example.splitwise.ui.theme.emerald_500
 
 @Composable
 fun HomeScreen(
@@ -36,53 +31,18 @@ fun HomeScreen(
     Scaffold(
         modifier = modifier
             .fillMaxSize()
-    ) {
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
+                .padding(innerPadding)
         ) {
             DashBoard()
-        }
-    }
-}
-
-@Composable
-fun DashBoard(
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primary)
-            .systemBarsPadding()
-            .padding(horizontal = Spacing.large, vertical = ScreenDimensions.verticalPadding)
-    ) {
-        Text(
-            text = stringResource(R.string.dashboard),
-            style = MaterialTheme.typography.headlineSmall,
-            color = MaterialTheme.colorScheme.onPrimary
-        )
-        Spacer(Modifier.height(Spacing.large))
-        BalanceView(
-            title = R.string.net_balance
-        )
-        Spacer(Modifier.height(Spacing.large))
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(ScreenDimensions.itemSpacing),
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            BalanceView(
-                icon = R.drawable.arrow_down,
-                title = R.string.total_owed,
+            ContentView(
+                onAddBill = {},
                 modifier = Modifier
-                    .weight(1f)
-            )
-            BalanceView(
-                icon = R.drawable.arrow_up,
-                title = R.string.total_owing,
-                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
                     .weight(1f)
             )
         }
@@ -90,40 +50,29 @@ fun DashBoard(
 }
 
 @Composable
-fun BalanceView(
-    icon: Int? = null,
-    title: Int,
+fun ContentView(
+    onAddBill: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier
-            .fillMaxWidth()
-            .background(emerald_500, shape = SplitWiseShapes.card)
-            .padding(ScreenDimensions.contentPadding)
+            .padding(start = ScreenDimensions.sectionSpacing, end = ScreenDimensions.sectionSpacing, top = ScreenDimensions.verticalPadding)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(Spacing.small),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            if (icon != null) {
-                Image(
-                    painter = painterResource(icon),
-                    contentDescription = null
-                )
-            }
-            Text(
-                text = stringResource(title),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onPrimary
-            )
-        }
-        Spacer(Modifier.height(Spacing.extraSmall))
-        Text(
-            text = "$0.00",
-            style = CurrencyLarge,
-            color = MaterialTheme.colorScheme.onPrimary
+        AppIconTextButton(
+            leadingIcon = R.drawable.plus_icon,
+            title = stringResource(R.string.add_bill),
+            onClick = {onAddBill()}
         )
+        Spacer(Modifier.height(Spacing.medium))
+//        EmptyBillView(
+//            onAddBill,
+//            modifier = Modifier
+//                .weight(1f)
+//        )
+        OwedView()
+        Spacer(Modifier.height(Spacing.large))
+        OwingView()
+        Spacer(Modifier.height(Spacing.large))
     }
 }
 
