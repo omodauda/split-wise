6be57fa270909968e176a6ea15b5d5c3aba9110f
omodauda <omodauda.dl@gmail.com>
2026-02-03 22:10:01 +0100

    step two destinations

---
+++ app/src/main/java/com/example/splitwise/ui/features/main/addBill/components/stepTwo/navigation/Destinations.kt
@@ -0,0 +1,9 @@
+package com.example.splitwise.ui.features.main.addBill.components.stepTwo.navigation
+
+enum class StepTwoDestination(
+    val route: String,
+    val label: String,
+) {
+    GROUPS("groups", "Groups"),
+    FRIENDS("friends", "Friends")
+}
