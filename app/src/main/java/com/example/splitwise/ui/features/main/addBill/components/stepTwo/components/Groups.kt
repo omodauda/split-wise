bc57c51c0b81303a917d326d7f1da119073fed0a
omodauda <omodauda.dl@gmail.com>
2026-02-06 06:59:31 +0100

    select group

--- app/src/main/java/com/example/splitwise/ui/features/main/addBill/components/stepTwo/components/Groups.kt
+++ app/src/main/java/com/example/splitwise/ui/features/main/addBill/components/stepTwo/components/Groups.kt
@@ -2,6 +2,7 @@ package com.example.splitwise.ui.features.main.addBill.components.stepTwo.compon
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,10 +14,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.pluralStringResource
@@ -24,15 +31,35 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.splitwise.R
+import com.example.splitwise.model.Group
 import com.example.splitwise.ui.theme.ComponentDimensions
 import com.example.splitwise.ui.theme.ScreenDimensions
 import com.example.splitwise.ui.theme.Spacing
 import com.example.splitwise.ui.theme.SplitWiseTheme
+import com.example.splitwise.ui.theme.emerald_50
 
 @Composable
 fun Groups(
     modifier: Modifier = Modifier
 ) {
+    var selectedGroup by rememberSaveable { mutableStateOf<String?>(null) }
+    val groups = listOf(
+        Group(
+            id = "1",
+            name = "Weekend Trip",
+            memberCount = 3
+        ),
+        Group(
+            id = "2",
+            name = "Tennis",
+            memberCount = 15
+        ),
+        Group(
+            id = "3",
+            name = "South Club",
+            memberCount = 2
+        )
+    )
     Column(
         modifier
     ) {
@@ -46,8 +73,17 @@ fun Groups(
             verticalArrangement = Arrangement.spacedBy(ScreenDimensions.itemSpacing),
             contentPadding = PaddingValues(bottom = Spacing.extraMedium)
         ) {
-            items(10) {
-                Group()
+            items(groups) { group ->
+                val isSelected = group.id == selectedGroup
+                Group(
+                    group,
+                    isSelected,
+                    modifier = Modifier
+                        .clickable(
+                            enabled = true,
+                            onClick = {selectedGroup = group.id}
+                        )
+                )
             }
         }
     }
@@ -55,6 +91,8 @@ fun Groups(
 
 @Composable
 fun Group(
+    group: Group,
+    isSelected: Boolean,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -62,10 +100,10 @@ fun Group(
         horizontalArrangement = Arrangement.spacedBy(ScreenDimensions.itemSpacing),
         modifier = modifier
             .fillMaxWidth()
-            .background(color = MaterialTheme.colorScheme.background)
+            .background(color = if (isSelected) emerald_50 else MaterialTheme.colorScheme.background)
             .border(
                 width = ComponentDimensions.borderWidthMedium, shape = MaterialTheme.shapes.large,
-                color = MaterialTheme.colorScheme.surfaceVariant
+                color = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surfaceVariant
             )
             .padding(Spacing.medium)
     ) {
@@ -76,7 +114,7 @@ fun Group(
         )
         Column{
             Text(
-                text = "Weekend Trip",
+                text = group.name,
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.onBackground
             )
@@ -84,8 +122,8 @@ fun Group(
             Text(
                 text = pluralStringResource(
                     R.plurals.member_count,
-                    3,
-                    3
+                    group.memberCount,
+                    group.memberCount
                 ),
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onSurfaceVariant
