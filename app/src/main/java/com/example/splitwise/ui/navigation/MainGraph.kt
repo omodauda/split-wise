4cd0d28069224d97349b081c783a94d15f725f75
omodauda <omodauda.dl@gmail.com>
2026-02-14 21:21:40 +0100

    use reset add bill ui state

--- app/src/main/java/com/example/splitwise/ui/navigation/MainGraph.kt
+++ app/src/main/java/com/example/splitwise/ui/navigation/MainGraph.kt
@@ -1,7 +1,5 @@
 package com.example.splitwise.ui.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
@@ -9,7 +7,6 @@ import androidx.navigation.navigation
 import com.example.splitwise.ui.features.auth.AuthViewModel
 import com.example.splitwise.ui.features.main.addBill.AddBillScreen
 import com.example.splitwise.ui.features.main.addBill.AddBillViewModel
-import com.example.splitwise.ui.features.main.addBill.AddBillViewModelFactory
 import com.example.splitwise.ui.features.main.addBillSuccess.AddBillSuccessScreen
 
 fun NavGraphBuilder.mainNavGraph(
@@ -17,9 +14,6 @@ fun NavGraphBuilder.mainNavGraph(
     authViewModel: AuthViewModel,
     addBillViewModel: AddBillViewModel
 ) {
-
-
-
     navigation(
         startDestination = Screen.Home.route,
         route = "main_graph"
@@ -32,17 +26,25 @@ fun NavGraphBuilder.mainNavGraph(
         }
         composable(route = Screen.AddBill.route){
             AddBillScreen(
-                goBack = {navController.popBackStack()},
-                goToAddBillSuccess = {navController.navigate(Screen.AddBillSuccess.route)},
+                goBack = {
+                    navController.popBackStack()
+                    addBillViewModel.resetState()
+                },
+                goToAddBillSuccess = {
+                    navController.navigate(Screen.AddBillSuccess.route)
+                },
                 addBillViewModel
             )
         }
         composable( route = Screen.AddBillSuccess.route){
             AddBillSuccessScreen(goHome = {
                 navController.navigate(Screen.Home.route) {
-                    popUpTo(Screen.Home.route)
+                    popUpTo(Screen.Home.route) {
+                        inclusive = true
+                    }
                     launchSingleTop =true
                 }
+                addBillViewModel.resetState()
             })
         }
     }
