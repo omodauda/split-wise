07ee38b55658aa39f0a79b0a89632b15e7f8ddf0
omodauda <omodauda.dl@gmail.com>
2026-02-21 15:50:16 +0100

    auth api

---
+++ app/src/main/java/com/example/splitwise/data/network/api/AuthApi.kt
@@ -0,0 +1,12 @@
+package com.example.splitwise.data.network.api
+
+import com.example.splitwise.data.network.model.LoginRequest
+import com.example.splitwise.data.network.model.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthApi {
+    @POST("auth/login")
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+}
