b4f34b439404a4b4d40969128c24c20157d3a10b
omodauda <omodauda.dl@gmail.com>
2026-02-21 15:50:45 +0100

    login using auth api

--- app/src/main/java/com/example/splitwise/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/splitwise/data/repository/AuthRepository.kt
@@ -1,12 +1,36 @@
 package com.example.splitwise.data.repository
 
+import android.util.Log
 import com.example.splitwise.data.local.IAuthPreference
+import com.example.splitwise.data.network.api.AuthApi
+import com.example.splitwise.data.network.model.ApiError
+import com.example.splitwise.data.network.model.LoginRequest
+import com.example.splitwise.data.network.model.LoginResponse
+import com.google.gson.Gson
 
-class AuthRepository(private val authPreference: IAuthPreference) {
+class AuthRepository(
+    private val authPreference: IAuthPreference,
+    private val authApi: AuthApi
+) {
     val isAuthenticated = authPreference.isAuthenticated
 
-    suspend fun authenticate() {
-        authPreference.setAuthenticated(true)
+    suspend fun login(data: LoginRequest): Result<LoginResponse?> {
+        return try {
+            val response = authApi.login(data)
+            if (response.isSuccessful && response.body() !== null) {
+                val loginResponse = response.body()
+                // TODO: save access token
+                authPreference.setAuthenticated(true)
+                Result.success(loginResponse)
+            } else {
+                val errorBody = response.errorBody()?.string()
+                val apiError = Gson().fromJson(errorBody, ApiError::class.java)
+                Log.d("Auth", "$apiError")
+                Result.failure(Exception(apiError.message))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
     suspend fun logout() {
