410642f252e0b7685fccd4363e827e55e10461bb
omodauda <omodauda.dl@gmail.com>
2026-03-16 10:40:14 +0100

    update fake app container

--- app/src/main/java/com/example/splitwise/mock/FakeAppContainer.kt
+++ app/src/main/java/com/example/splitwise/mock/FakeAppContainer.kt
@@ -2,9 +2,12 @@ package com.example.splitwise.mock
 
 import com.example.splitwise.data.local.IAuthPreference
 import com.example.splitwise.data.network.api.AuthApi
+import com.example.splitwise.data.network.api.BillApi
 import com.example.splitwise.data.network.api.FriendApi
 import com.example.splitwise.data.network.model.AuthData
 import com.example.splitwise.data.network.model.AuthUserData
+import com.example.splitwise.data.network.model.CreateBillRequest
+import com.example.splitwise.data.network.model.CreateBillResponse
 import com.example.splitwise.data.network.model.FriendInviteResponse
 import com.example.splitwise.data.network.model.GetFriendsResponse
 import com.example.splitwise.data.network.model.GetPendingInvitesResponse
@@ -14,6 +17,7 @@ import com.example.splitwise.data.network.model.PaginationMetaData
 import com.example.splitwise.data.network.model.SendFriendInviteRequest
 import com.example.splitwise.data.network.model.SignupRequest
 import com.example.splitwise.data.repository.AuthRepository
+import com.example.splitwise.data.repository.BillRepository
 import com.example.splitwise.data.repository.FriendRepository
 import com.example.splitwise.data.repository.InviteRepository
 import com.example.splitwise.di.IAppContainer
@@ -130,6 +134,13 @@ class FakeFriendApi : FriendApi {
     }
 }
 
+class FakeBillApi: BillApi {
+    override suspend fun addBill(request: CreateBillRequest): Response<CreateBillResponse> {
+        TODO("Not yet implemented")
+    }
+
+}
+
 class FakeAppContainer : IAppContainer {
 
     // First, implement the missing 'authApi' member.
@@ -146,5 +157,10 @@ class FakeAppContainer : IAppContainer {
         get() = TODO("Not yet implemented")
 
     override val inviteRepository: InviteRepository
+        get() = TODO("Not yet implemented")
+
+    override val billApi: BillApi = FakeBillApi()
+
+    override val billRepository: BillRepository
         get() = TODO("Not yet implemented")
 }
