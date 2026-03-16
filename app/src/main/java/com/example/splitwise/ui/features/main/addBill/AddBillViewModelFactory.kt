4c481053bed67fdf102965c7986fe5f777bd0818
omodauda <omodauda.dl@gmail.com>
2026-03-16 10:38:45 +0100

    use bill repo in bill view model factory

--- app/src/main/java/com/example/splitwise/ui/features/main/addBill/AddBillViewModelFactory.kt
+++ app/src/main/java/com/example/splitwise/ui/features/main/addBill/AddBillViewModelFactory.kt
@@ -2,12 +2,14 @@ package com.example.splitwise.ui.features.main.addBill
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.splitwise.data.repository.AuthRepository
+import com.example.splitwise.data.repository.BillRepository
 
-class AddBillViewModelFactory: ViewModelProvider.Factory {
+class AddBillViewModelFactory(private val billRepository: BillRepository): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AddBillViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return AddBillViewModel() as T
+            return AddBillViewModel(billRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
