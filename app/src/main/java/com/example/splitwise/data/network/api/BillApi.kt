fa555f0f109aa16cb1400ac5f2b5989f2b608c87
omodauda <omodauda.dl@gmail.com>
2026-03-17 13:44:56 +0100

    bill api

--- app/src/main/java/com/example/splitwise/data/network/api/BillApi.kt
+++ app/src/main/java/com/example/splitwise/data/network/api/BillApi.kt
@@ -2,15 +2,31 @@ package com.example.splitwise.data.network.api
 
 import com.example.splitwise.data.network.model.CreateBillRequest
 import com.example.splitwise.data.network.model.CreateBillResponse
+import com.example.splitwise.data.network.model.GetOwedBillsResponse
+import com.example.splitwise.data.network.model.GetOwingBillsResponse
+import com.example.splitwise.data.network.model.GetPendingInvitesResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface BillApi {
-
     @POST("bill/add")
     suspend fun addBill(
         @Body request: CreateBillRequest
     ): Response<CreateBillResponse>
+
+    @GET("bill/owed")
+    suspend fun getOwedBills(
+        @Query("cursorId") cursorId: String?,
+        @Query("limit") limit: Int?
+    ): Response<GetOwedBillsResponse>
+
+    @GET("bill/owing")
+    suspend fun getOwingBills(
+        @Query("cursorId") cursorId: String?,
+        @Query("limit") limit: Int?
+    ): Response<GetOwingBillsResponse>
 
 }
