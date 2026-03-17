ca55735e52ecb9dbd3ef332e9f78cf52554ca35e
omodauda <omodauda.dl@gmail.com>
2026-03-17 13:46:07 +0100

    bills model

--- app/src/main/java/com/example/splitwise/data/network/model/BillModel.kt
+++ app/src/main/java/com/example/splitwise/data/network/model/BillModel.kt
@@ -1,6 +1,5 @@
 package com.example.splitwise.data.network.model
 
-import com.example.splitwise.model.User
 import java.util.Date
 
 data class CreateBillRequest(
@@ -17,7 +16,35 @@ data class BillSplit(
     val percentage: Double,
     val amount: Int
 )
-
 data class CreateBillResponse(
     val message: String
+)
+
+data class GetOwedBillsResponse(
+    val data: List<OwedBill>,
+    val meta: PaginationMetaData
+)
+data class OwedBill(
+    val id: String,
+    val amount: Int,
+    val user: User
+)
+data class User(
+    val id: String,
+    val fullName: String,
+    val avatar: String?
+)
+
+data class GetOwingBillsResponse(
+    val data: List<OwingBill>,
+    val meta: PaginationMetaData
+)
+data class OwingBill(
+    val id: String,
+    val amount: Int,
+    val bill: Bill
+)
+data class Bill(
+    val id: String,
+    val paidBy: User
 )
