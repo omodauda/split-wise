0f8d0f1070a0d3f8b309749de179b49a2975b8c1
omodauda <omodauda.dl@gmail.com>
2026-03-18 17:44:59 +0100

    pay bill

--- app/src/main/java/com/example/splitwise/ui/features/main/home/components/SettleUpModal.kt
+++ app/src/main/java/com/example/splitwise/ui/features/main/home/components/SettleUpModal.kt
@@ -1,5 +1,6 @@
 package com.example.splitwise.ui.features.main.home.components
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -20,21 +21,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import com.example.splitwise.R
+import com.example.splitwise.data.network.model.OwingBill
+import com.example.splitwise.data.network.model.PayBillRequest
 import com.example.splitwise.ui.components.AppDropdownPicker
 import com.example.splitwise.ui.components.AppTextButton
 import com.example.splitwise.ui.components.AppTextField
 import com.example.splitwise.ui.theme.ScreenDimensions
 import com.example.splitwise.ui.theme.Spacing
 import com.example.splitwise.ui.theme.SplitWiseShapes
+import com.example.splitwise.utils.formatFromCents
+import java.util.UUID
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettleUpModal(
     sheetState: SheetState,
-    onDismissRequest: () -> Unit
+    onDismissRequest: () -> Unit,
+    bill: OwingBill?,
+    payBill: (PayBillRequest) -> Unit,
 ) {
     ModalBottomSheet(
-        onDismissRequest = {onDismissRequest()},
+        onDismissRequest = { onDismissRequest() },
         sheetState = sheetState,
         containerColor = MaterialTheme.colorScheme.background
     ) {
@@ -45,19 +53,36 @@ fun SettleUpModal(
                     shape = SplitWiseShapes.bottomSheet
                 )
         ) {
-            RecordPaymentModalHeader(onDismiss = {onDismissRequest()}, title = R.string.settle_up)
-            SettleUpModalContent()
+            RecordPaymentModalHeader(onDismiss = { onDismissRequest() }, title = R.string.settle_up)
+            SettleUpModalContent(bill, payBill)
         }
     }
 }
 
 @Composable
 fun SettleUpModalContent(
+    bill: OwingBill?,
+    payBill: (PayBillRequest) -> Unit,
     modifier: Modifier = Modifier
 ) {
     var settlementAmount by remember { mutableStateOf("") }
-    val paymentMethods = listOf(R.string.cash, R.string.credit_card, R.string.bank_transfer)
+    val paymentMethods = listOf(R.string.cash, R.string.bank_transfer)
     var selectedPaymentMethod by remember { mutableStateOf(paymentMethods[0]) }
+
+    val remainingAmount = if (bill !== null) {
+        bill.amount - bill.paidAmount
+    } else 0
+
+    fun handlePay() {
+        val payload = PayBillRequest(
+            splitId = bill!!.id,
+            idempotencyKey = UUID.randomUUID().toString(),
+            amount = ((settlementAmount.toDoubleOrNull() ?: 0.0) * 100).roundToInt(),
+            method = if (selectedPaymentMethod == R.string.cash) "Cash" else "BankTransfer"
+        )
+        Log.d("pay", payload.toString())
+        payBill(payload)
+    }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -66,8 +91,8 @@ fun SettleUpModalContent(
     ) {
         PaymentDetailView(
             title = R.string.you_owe,
-            amount = "$300.00",
-            fullName = "David Brown",
+            amount = formatFromCents(remainingAmount),
+            fullName = bill?.bill?.paidBy?.fullName ?: "",
             owe = true
         )
         Spacer(Modifier.height(ScreenDimensions.sectionSpacing))
@@ -95,7 +120,8 @@ fun SettleUpModalContent(
         Spacer(Modifier.height(ScreenDimensions.sectionSpacing))
         AppTextButton(
             title = stringResource(R.string.confirm_payment),
-            onClick = {}
+            onClick = { handlePay() },
+            enabled = settlementAmount.isNotEmpty()
         )
     }
 }
