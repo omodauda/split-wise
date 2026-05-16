0384e6b17f77e04ca7930c731c431736e8f61a86
omodauda <omodauda.dl@gmail.com>
2026-05-16 13:56:21 +0100

    optimize friends screen

--- app/src/main/java/com/omodauda/splitwise/ui/features/main/friends/components/InviteFriendModal.kt
+++ app/src/main/java/com/omodauda/splitwise/ui/features/main/friends/components/InviteFriendModal.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -99,6 +101,7 @@ fun InviteFriendModal(
                     color = MaterialTheme.colorScheme.background,
                     shape = SplitWiseShapes.bottomSheet
                 )
+                .verticalScroll(rememberScrollState())
         ) {
             InviteFriendModalHeader(onDismiss = {onDismissRequest()}, title = R.string.invite_friends)
             InviteFriendModalContent(
